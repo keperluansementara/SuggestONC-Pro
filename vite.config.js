bba184ba027da8c2669fd5f2282a7c21361db1f7
keperluansementara <keperluansementara@gmail.com>
2026-03-12 23:44:43 +0700

    update Fitur

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isGithubPages = process.env.GITHUB_PAGES === "true"
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: isGithubPages ? '/SuggestONC-Pro/' : '/',
   server: {
     host: true,
     port: 5173,
